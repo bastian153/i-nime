da694f20f35404bd68463bd1cb155b6f0959cb19
Ruben <rc17.dt@gmail.com>
2015-08-04 23:32:48 -0700

    fixing header gradient color and first attempt to insert video

---
+++ js/my_java_script.js
@@ -0,0 +1,27 @@
+$( ".my_header_links" ).hover(
+function() {
+	$(this).css("color","rgb(140, 96, 224)"); 
+},
+function(){
+	$(this).css("color","rgb(240, 255, 0)"); 
+});
+
+$(document).ready(function() {
+    my_fade($(".pic_element").first());
+});
+
+function my_fade( elem )
+{
+ elem.delay()
+     .fadeIn(0)
+     .delay(7500)
+     .fadeOut( 
+               function(){
+				   if(elem.next().length)
+				   {
+				    	my_fade( elem.next() );	   
+				   }else{
+						my_fade( $(".pic_element").first() );   
+				   }}
+             );
+}
